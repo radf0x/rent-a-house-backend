1dc32984f2cd07fce8d1c3f5c6f5e0203388d783
Ravic Poon <ravicpoon@gmail.com>
2022-07-06 17:42:04 +0800

    added tests to cover unauthorized use cases

--- spec/requests/rentals_spec.rb
+++ spec/requests/rentals_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Rentals", type: :request do
-  describe "GET /api/rentals" do
+  describe 'when authorized' do
+    let!(:application) { FactoryBot.create(:doorkeeper_application) }
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:auth_token) { FactoryBot.create(:doorkeeper_access_token, application: application, resource_owner_id: user.id).token }
+  
     before do
       10.times { FactoryBot.create(:property, :taipei_city) }
       10.times { FactoryBot.create(:property, :new_taipei_city) }
     end
-
-    context 'when authorized' do
-      let!(:application) { FactoryBot.create(:doorkeeper_application) }
-      let!(:user) { FactoryBot.create(:user) }
-      let!(:auth_token) { FactoryBot.create(:doorkeeper_access_token, application: application, resource_owner_id: user.id).token }
-
+  
+    describe 'GET /api/rentals/:id' do
+      let(:property) { FactoryBot.create(:property, :taipei_city) }
+  
+      it 'respond with the correct property' do
+        get "/api/rentals/#{property.id}", headers: with_user_auth_headers(auth_token)
+        expect(response.parsed_body['id']).to eq property.id
+      end
+  
+      it 'respond with 404 with not found error message' do
+        get "/api/rentals/#{-property.id}", headers: with_user_auth_headers(auth_token)
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body['error']).to eq "Cound't find rental"
+      end
+    end
+  
+    describe "GET /api/rentals" do
       after do
         expect(response).to have_http_status(:ok)
         expect(response.parsed_body['pagination']).to include_json(
@@ -20,7 +35,7 @@ RSpec.describe "Rentals", type: :request do
           total: /\d/
         )
       end
-
+  
       context 'with a single filter' do
         it 'respond with properties that have rooms within the filter value' do
           num_of_rooms = rand(1..5)
@@ -29,7 +44,7 @@ RSpec.describe "Rentals", type: :request do
             expect(property['rooms']).to be <= num_of_rooms
           end
         end
-
+  
         it 'respond with properties that are in Taipei City' do
           get '/api/rentals', params: { city: '台北市' }, headers: with_user_auth_headers(auth_token)
           response.parsed_body['properties'].each do |property|
@@ -37,17 +52,17 @@ RSpec.describe "Rentals", type: :request do
           end
         end
       end
-
+  
       context 'with a composite filter' do
         let!(:districts) { %w[中正區 中山區] }
         let!(:query_params) { { city: '台北市', districts: districts, mrt_line: '台北車站' , max_rent: 30000} }
-
+  
         before do
           districts.each do |district|
             FactoryBot.create(:property, :taipei_city, price: 30000.to_d, district: district, mrt_line: query_params[:mrt_line])
           end
         end
-
+  
         it 'respond with properties that matches the filter' do
           get '/api/rentals', params: query_params, headers: with_user_auth_headers(auth_token)
           response.parsed_body['properties'].each do |property|
@@ -58,6 +73,23 @@ RSpec.describe "Rentals", type: :request do
           end
         end
       end
+    end
+  end
+
+  describe 'when not authorized' do
+    shared_examples_for 'GET /api/rentals' do
+      it 'respond with http 401' do
+        get endpoint
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+    
+    it_behaves_like 'GET /api/rentals' do
+      let(:endpoint) { '/api/rentals' }
+    end
+
+    it_behaves_like 'GET /api/rentals' do
+      let(:endpoint) { '/api/rentals/1' }
     end
   end
 end
