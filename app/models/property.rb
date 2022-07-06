0178fd60de28c2b6c66fe4c5e79e595ca0ce445d
Ravic Poon <ravicpoon@gmail.com>
2022-07-06 17:11:43 +0800

    added pagination config and query scopes to Property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Property < ApplicationRecord
+  paginates_per 6
+
+  scope :by_city, ->(city) { where(city: city) unless city.blank? }
+  scope :by_districts, ->(districts) { where(district: districts) unless districts.empty? }
+  scope :by_rooms, ->(rooms) { where('rooms <= ?', rooms) unless rooms == 0 }
+  scope :by_rent, ->(min, max) { where('price between ? and ?', min.to_i, max.to_i) unless (min + max).zero? }
+  scope :by_mrt_line, ->(mrt_line) { where(mrt_line: mrt_line) unless mrt_line.blank? }
 end
