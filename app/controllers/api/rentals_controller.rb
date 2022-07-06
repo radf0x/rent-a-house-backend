048ff0fa42e54aca7f92ad5af69b6dfaa1f2affc
Ravic Poon <ravicpoon@gmail.com>
2022-07-06 17:41:29 +0800

    handle show rental info api

--- app/controllers/api/rentals_controller.rb
+++ app/controllers/api/rentals_controller.rb
@@ -1,38 +1,43 @@
 # frozen_string_literal: true
 
+# TODO: add ActiveModel Serializer for json results
+# TODO: add request param validators
 class Api::RentalsController < ApiController
   before_action :admin_only, only: %i[create update delete]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_error
 
   def index
-    collections = Property.by_city(query_params[:city] || '')
-      .by_districts(query_params[:districts] || [])
-      .by_rooms(query_params[:rooms].to_i.abs)
-      .by_rent(query_params[:min_rent].to_i.abs, query_params[:max_rent].to_i.abs)
-      .by_mrt_line(query_params[:mrt_line] || '')
-      .page(query_params[:page].to_i.abs || 1)
+    collections = Property.by_city(filter_params[:city] || '')
+      .by_districts(filter_params[:districts] || [])
+      .by_rooms(filter_params[:rooms].to_i.abs)
+      .by_rent(filter_params[:min_rent].to_i.abs, filter_params[:max_rent].to_i.abs)
+      .by_mrt_line(filter_params[:mrt_line] || '')
+      .page(filter_params[:page].to_i.abs || 1)
 
     render json: { properties: collections, pagination: pagination_data_from(collections) }, status: 200
   end
 
   def show
-    puts "SHOW"
+    property = Property.find_by!(id: params[:id])
+    render json: property, status: 200
   end
 
   def create
-    puts "hihi"
-    render json: { user: current_user }, status: 200
+    # TODO
   end
 
   def update
+    # TODO
   end
 
   def delete
+    # TODO
   end
 
   private
 
-  def query_params
-    @query_params ||= params.permit(:city, :rooms, :mrt_line, :min_rent, :max_rent, :page, districts: [])
+  def filter_params
+    @filter_params ||= params.permit(:city, :rooms, :mrt_line, :min_rent, :max_rent, :page, districts: [])
   end
 
   def pagination_data_from(collection)
@@ -43,5 +48,9 @@ class Api::RentalsController < ApiController
       next: next_page,
       total: total_page
     }
+  end
+
+  def render_record_not_found_error(error)
+    render json: { error: "Cound't find rental" }, status: :not_found
   end
 end
